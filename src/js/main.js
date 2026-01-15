b78edaf9b34e6ba34143164687c9268d26ac246a
Egor <vlesueji@gmail.com>
2026-01-15 18:51:36 +0300

    add close bb

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ const mobileMenu = document.querySelector('.mobile-menu-cover');
 
 function switchMenu(event){
   mobileMenu.classList.toggle('visually-hidden');
+  burgerButton.classList.toggle('burger-button-close');
   return
 }
 
