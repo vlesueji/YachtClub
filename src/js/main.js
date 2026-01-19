0ba54bc85f1239289bbf087f807ba896886083ca
Egor <vlesueji@gmail.com>
2026-01-19 17:58:18 +0300

    add modal open class

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 const burgerButton = document.querySelector('.burger-button');
 const mobileMenu = document.querySelector('.mobile-menu-cover');
-const headerLogo = document.querySelector('.header__logo');
 
 function switchMenu(event){
   mobileMenu.classList.toggle('visually-hidden');
   burgerButton.classList.toggle('burger-button-close');
+  document.body.classList.toggle('modal-open');
   return
 }
 
